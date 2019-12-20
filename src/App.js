fbec81328c3a06e411a8eebcc88b85458fcdb1b7
iandart66 <ian.dart@idc.technology>
2019-12-20 20:38:28 +0000

    Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/navBar";
+import home from "./components/home";
+import broadband from "./components/broadband";
+import axios from "axios";
+//import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+
+  async componentDidMount() {
+    const promise = await axios
+      .post("https://api.gamma.co.uk/auth/token", {
+        content_type: "application/x-www-form-urlencoded",
+        grant_type: "promise",
+        username: "IanDart1",
+        password: "Migj7heoGI"
+      })
+      .catch();
+    console.log(promise);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <NavBar />
+          <main className="container-fluid">
+            <Switch>
+              <Route path="/broadband" Component={broadband} />
+              ,<Route path="/" Component={home} />
+            </Switch>
+          </main>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
